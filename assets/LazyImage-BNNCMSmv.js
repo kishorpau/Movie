9f5cef6c16cbb6b5ec74940be6b125c30f998a29
kishorpau <kishorpau@users.noreply.github.com>
2024-05-11 21:49:59 +0000

    deploy: 213e2fae7d661d22127e8541cf8aeaa58b83d2e0

---
+++ assets/LazyImage-BNNCMSmv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-CwRlnWnL.js";const o=({src:t,alt:r,style:s,className:a})=>e.jsx("img",{src:t,alt:r,style:s,className:a});export{o as default};
--- assets/LazyImage-CDq4XlVT.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-ChrELUSd.js";const o=({src:t,alt:r,style:s,className:a})=>e.jsx("img",{src:t,alt:r,style:s,className:a});export{o as default};
