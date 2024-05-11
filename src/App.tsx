df493caf93bbf89085bfb7514527a9a0815b77aa
kishorpau <kishorpau66@gmail.com>
2024-05-12 02:59:16 +0545

    Css Changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Movies from "./pages/explore/Movies";
 import Details from "./pages/Details/Details";
 import TvSeries from "./pages/explore/TvSeries";
 import SearchResultPage from "./pages/SearchResultPage";
+import About from "./pages/About";
+import Error from "./pages/Error";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/explore/Movies" element={<Movies />} />
         <Route path="/explore/Tv" element={<TvSeries />} />
         <Route path="/search-results" element={<SearchResultPage />} />
+        <Route path="/About" element={<About />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
   );
